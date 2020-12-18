ba5b930f64e70c583cb09c5f86e04dfa62e00113
Huang0616 <yh3025@nyu.edu>
2020-12-19 02:26:42 +0800

    fixed two nets

--- repair_badnet1_fine_prune.py
+++ repair_badnet1_fine_prune.py
@@ -4,10 +4,11 @@ import h5py
 import numpy as np
 import matplotlib.pyplot as plt
 
-data_filename_val = str(sys.argv[1])
+data_filename_validation = str(sys.argv[1])
 data_filename_poisoned = str(sys.argv[2])
 data_filename_test = str(sys.argv[3])
 model_filename = str(sys.argv[4])
+
 
 def data_loader(filepath):
     data = h5py.File(filepath, 'r')
@@ -17,125 +18,140 @@ def data_loader(filepath):
 
     return x_data, y_data
 
+
 def data_preprocess(x_data):
     return x_data/255
 
+
 def main():
+    x_validation, y_validation = data_loader(data_filename_validation)
+    x_validation = data_preprocess(x_validation)
+
     x_test_poi, y_test_poi = data_loader(data_filename_poisoned)
     x_test_poi = data_preprocess(x_test_poi)
-
-    x_test_val, y_test_val = data_loader(data_filename_val)
-    x_test_val = data_preprocess(x_test_val)
 
     x_test, y_test = data_loader(data_filename_test)
     x_test = data_preprocess(x_test)
 
     bd_model = keras.models.load_model(model_filename)
+    layer_model = keras.models.Model(inputs=bd_model.input, outputs=bd_model.get_layer('conv_4').output)
 
-    layer_model = keras.models.Model(inputs = bd_model.input, outputs = bd_model.get_layer('conv_4').output)
+    # active = layer_model.predict(x_test_poi)
+    # # for ploting
+    # activation_visual = np.mean(active, axis=0)
+    # activation = np.mean(active, axis=(0,1,2))
 
-    conv5_num = 80
-    activation = np.zeros((len(x_test_poi), conv5_num))
-    activation_val = np.zeros((len(x_test_val), conv5_num))
+    active_val = layer_model.predict(x_validation)
+    # activation_val_visual = np.mean(active_val, axis=0)
+    activation_val = np.mean(active_val, axis=(0, 1, 2))
 
-    active = layer_model.predict(x_test_poi)
-    activation_visual = np.mean(active, axis=0)
-    activation = np.mean(active, axis=(0,1,2))
+    # # plotting figures of activation given backdoor inputs
+    # fig=plt.figure(figsize=(12, 16))
+    # columns = 10
+    # rows = 8
+    # for i in range(1, columns*rows + 1):
+    #     img = activation_visual[:,:,i-1]
+    #     img = img.reshape(3, 4)
+    #     fig.add_subplot(rows, columns, i)
+    #     plt.imshow(img)
+    # plt.title('Backdoored data neuron activations')
+    # plt.show()
 
-    active_val = layer_model.predict(x_test_val)
-    activation_val_visual = np.mean(active_val, axis=0)
-    activation_val = np.mean(active_val, axis=(0,1,2))
+    # # plotting figures of activation given clean inputs
+    # fig=plt.figure(figsize=(12, 16))
+    # columns = 10
+    # rows = 8
+    # for i in range(1, columns*rows + 1):
+    #     img = activation_val_visual[:,:,i-1]
+    #     img = img.reshape(3, 4)
+    #     fig.add_subplot(rows, columns, i)
+    #     plt.imshow(img)
+    # plt.title('Clean data neuron activations')
+    # plt.show()
 
-    fig=plt.figure(figsize=(12, 16))
-    columns = 10
-    rows = 8
-    for i in range(1, columns*rows + 1):
-        img = activation_visual[:,:,i-1]
-        img = img.reshape(3, 4)
-        fig.add_subplot(rows, columns, i)
-        plt.imshow(img)
-    plt.title('Backdoored data neuron activations')
-    plt.show()
+    # # ploting fraction of neuron pruned to accuracy curve
+    # prune_acc = [0]*80
+    # fine_pruning_acc = [0]*80
+    # active_sort = np.argsort(activation_val);
+    # for prune_num in range(80):
+    #   model = keras.models.load_model(model_filename)
+    #   for i in range(prune_num):
+    #     channel = active_sort[i]
+    #     weight = bd_model.get_layer('conv_4').get_weights()
+    #     weight[1][channel] = -100000
+    #     bd_model.get_layer('conv_4').set_weights(weight)
 
-    fig=plt.figure(figsize=(12, 16))
-    columns = 10
-    rows = 8
-    for i in range(1, columns*rows + 1):
-        img = activation_val_visual[:,:,i-1]
-        img = img.reshape(3, 4)
-        fig.add_subplot(rows, columns, i)
-        plt.imshow(img)
-    plt.title('Clean data neuron activations')
-    plt.show()
+    #     clean_label_p = np.argmax(bd_model.predict(x_validation), axis=1)
+    #     prune_acc[i] = np.mean(np.equal(clean_label_p, y_validation))*100
 
-    activation_sort = np.argsort(activation)[:60]
-    activation_val_sort = np.argsort(activation_val)[:60]
+    # x_axis = np.linspace(0,80,80)
+    # plt.plot(x_axis, prune_acc)
+    # plt.xlabel("number of neurons pruned")
+    # plt.ylabel("prediction accuracy on validation data")
 
-    bd_activation = []
-    not_in_both = []
-    bd_first = []
+    activation_val_sort = np.argsort(activation_val)
 
-    for i in range(80):
-        if not np.isin(i, activation_val_sort) and not np.isin(i, activation_sort):
-            not_in_both.append(i)
+    # the index array of three kinds of neurons
+    clean_mask = []
+    # backdoor_mask = []
+    number_clean = 20
+    for i in range(number_clean):
+        clean_mask.append(activation_val_sort[80 - number_clean + i])
 
-    for i in activation_sort:
-        if not np.isin(i, activation_val_sort):
-            bd_activation.append(i)
-        else:
-            bd_first.append(i)
+        # pruning
+    pruned_model = keras.models.load_model(model_filename)
+    for i in range(80 - number_clean):
+        channel = activation_val_sort[i]
+        weight = pruned_model.get_layer('conv_4').get_weights()
+        weight[1][channel] = -100000
+        pruned_model.get_layer('conv_4').set_weights(weight)
 
-    for i in not_in_both:
-        channel = i
-        weight = bd_model.get_layer('conv_4').get_weights()
-        weight[1][channel] = -10000
-        bd_model.get_layer('conv_4').set_weights(weight)
-
-    for i in bd_activation:
-        channel = i
-        weight = bd_model.get_layer('conv_4').get_weights()
-        weight[1][channel] = -10000
-        bd_model.get_layer('conv_4').set_weights(weight)
-
-    for i in range(48):
-        channel = bd_first[i]
-        weight = bd_model.get_layer('conv_4').get_weights()
-        weight[1][channel] = -10000
-        bd_model.get_layer('conv_4').set_weights(weight)
-
-    bd_model.fit(x_test_val, y_test_val, epochs=5)
-    clean_label = np.argmax(bd_model.predict(x_test), axis=1)
-    class_accu = np.mean(np.equal(clean_label, y_test))*100
+    pruned_model.fit(x_validation, y_validation, epochs=5)
+    clean_label = np.argmax(pruned_model.predict(x_test), axis=1)
+    class_accu = np.mean(np.equal(clean_label, y_test)) * 100
     print('Classification accuracy on test data after fine pruning: ', class_accu)
 
-    poi_label = np.argmax(bd_model.predict(x_test_poi), axis=1)
-    class_accu_po = np.mean(np.equal(poi_label, y_test_poi))*100
+    poi_label = np.argmax(pruned_model.predict(x_test_poi), axis=1)
+    class_accu_po = np.mean(np.equal(poi_label, y_test_poi)) * 100
     print('Classification accuracy on the poison data after fine pruning: ', class_accu_po)
 
-    active = layer_model.predict(x_test_poi)
-    bd_mean = np.mean(active)
+    # using unpruned model to classify the posion data
+    a_poison = layer_model.predict(x_test_poi)
+    a_clean = layer_model.predict(x_test)
 
-    active_final = layer_model.predict(x_test_val)
-    clean_mean = np.mean(active_final)
+    # using validation data for metric -> activation_val
 
-    active_test_data = layer_model.predict(x_test)
-    active_mean_test_data = np.mean(active_test_data, axis=(1,2,3))
+    detected = 0
+    threshold = 0.8
+    for i in range(len(x_test_poi)):
+        if np.mean(np.mean(a_poison[i], axis=(0, 1))[clean_mask] - activation_val[clean_mask]) > threshold:
+            detected += 1
+    print('There are ', detected, ' poisoned images detected in the poison data set')
+    print("Dectection accuracy is ", detected / len(x_test_poi))
 
-    count = 0
-    index_for_poisoned_image = []
-    for i in range(len(active_mean_test_data)):
-        if active_mean_test_data[i] > (bd_mean + clean_mean) / 2 * 0.95:
-            count += 1  
-            index_for_poisoned_image.append(i)
+    detected_clean = 0
+    for i in range(len(x_test)):
+        if np.mean(np.mean(a_clean[i], axis=(0, 1))[clean_mask] - activation_val[clean_mask]) < threshold:
+            detected_clean += 1
+    print('There are ', detected_clean, ' clean images detected in the clean data set')
+    print("Dectection accuracy is ", detected_clean / len(x_test))
 
-    print('There are ', count, ' poisoned images')
+    pred = [0] * len(x_test)
+    index_for_clean_image = []
+    for i in range(len(x_test)):
+        if np.mean(np.mean(a_clean[i], axis=(0, 1))[clean_mask] - activation_val[clean_mask]) < threshold:
+            index_for_clean_image.append(i)
+        else:
+            pred[i] = -1
 
-    for i in index_for_poisoned_image:
-        clean_label[i] = -1
-        
-    print('labels for the test data, -1 mean the data is poisoned: ')    
-    print(clean_label)
-    return clean_label
+    labels = np.argmax(pruned_model.predict(x_test[index_for_clean_image]), axis=1)
+    for i in range(len(index_for_clean_image)):
+        pred[index_for_clean_image[i]] = labels[i]
+
+    print('labels for the test data(in nets one only contains clean data), -1 mean the data is poisoned')
+    print(pred)
+    return pred
+
 
 if __name__ == '__main__':
     main()
